5341458efd6c2bb36b294df43133452f00f41539
fox <mfulbri@wgu.edu>
2025-01-24 13:46:54 -0500

    setting up files/services for app

---
+++ world-map-app/src/app/countryinfo/countryinfo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-countryinfo',
+  imports: [],
+  templateUrl: './countryinfo.component.html',
+  styleUrl: './countryinfo.component.css'
+})
+export class CountryinfoComponent {
+
+}
---
+++ world-map-app/src/app/worldmap/worldmap.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-worldmap',
+  imports: [],
+  templateUrl: './worldmap.component.html',
+  styleUrl: './worldmap.component.css'
+})
+export class WorldmapComponent {
+
+}
